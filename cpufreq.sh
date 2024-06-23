409d02e431a09658bb782cd521ab68ee40c75f62
Guilherme Amaral <guiopen.dev@gmail.com>
2024-06-23 14:55:25 -0300

    Update cpufreq.sh
    changes the secondary cpu governor from ondemand to conservative - fixes #1

--- cpufreq.sh
+++ cpufreq.sh
@@ -11,6 +11,6 @@ echo 408000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
 # 3 - tuning secondary cpu core
-echo ondemand > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo 408000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
 echo 1200000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
