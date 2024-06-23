ba9c9f7518fc8a255f8c684650438b97e56dbda2
Guilherme Amaral <guiopen.dev@gmail.com>
2024-06-23 18:11:36 -0300

    Update cpufreq.sh
    After further investigation, I found two things:
    
    1 - some emulators like PPSSPP and Drastic can use more than one core and limiting the system to only two cores decreases performance in these emulators. Increasing to 3 cores already allows us to extract everything from these emulators, without the need to activate the fourth core that is barely used.
    2 - In the TSP, the frequency limits and the CPU governor are shared between all cores, so we cannot limit the secondary CPU frequency to 1200mhz because that also limits the main CPU core.
    
    Because of this, I increased the number of cores used from 2 to 3 and removed the secondary core frequency limitation.

--- cpufreq.sh
+++ cpufreq.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-# 1 - disabling unecessary cpu cores
+# 1 - disabling unecessary cpu core
 echo 1 > /sys/devices/system/cpu/cpu0/online
 echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
 echo 0 > /sys/devices/system/cpu/cpu3/online
-echo 0 > /sys/devices/system/cpu/cpu2/online
 
-# 2 - tuning main cpu core
+# 2 - tuning cpu cores
 echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 408000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1800000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-
-# 3 - tuning secondary cpu core
-echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo 408000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
-echo 1200000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
