bbb3f07137e05f2c09858dd6bbe22cb9298bf9b4
guiopen <guiopen.dev@gmail.com>
2024-07-07 04:44:40 -0300

    add powersave and performance profiles
    remove core limiter from defaulot profile

--- cpufreq_powersave.sh
+++ cpufreq_powersave.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 408000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 echo 1608000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
 
